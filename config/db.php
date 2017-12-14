510f700f715353b68cd44a93cbc89e7456ccfd50
wlf <wendreoofernandes@gmail.com>
2017-12-14 00:32:23 -0200

    1st commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'development' =>
+        [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'lab',
+            'username' => 'root',
+            'password' => '1993',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci'
+        ]
+];
