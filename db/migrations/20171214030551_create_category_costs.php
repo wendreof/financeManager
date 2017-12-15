9f029c56468086e72bb428622bbaf3865387b8bc
wlf <wendreoofernandes@gmail.com>
2017-12-14 22:18:00 -0200

    generating category_costs

--- db/migrations/20171214030551_create_category_costs.php
+++ db/migrations/20171214030551_create_category_costs.php
@@ -4,29 +4,18 @@ use Phinx\Migration\AbstractMigration;
 
 class CreateCategoryCosts extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
+    public function up()
     {
+        $this->table('category_costs')
+             ->addColumn('name', 'string')
+             ->addColumn('created_at', 'datetime')
+             ->addColumn('update_at', 'datetime')
+             ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('category_costs');
 
     }
 }
