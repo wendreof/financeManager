9eec94aad1804f26ce3b81b6cfde59adc728f2a5
wlf <wendreoofernandes@gmail.com>
2017-12-14 23:16:23 -0200

    automation of migrations

---
+++ migrate-seed.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wlf
+ * Date: 14/12/17
+ * Time: 23:12
+ */
+
+exec(__DIR__ . '/vendor/bin/phinx rollback -t=0');
+exec(__DIR__ . '/vendor/bin/phinx migrate');
+exec(__DIR__ . '/vendor/bin/phinx seed:run');
