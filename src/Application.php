6950cf0b5d33618f1fe4e17cbebb92c5f00c3d68
wlf <wendreoofernandes@gmail.com>
2018-01-15 21:58:35 -0200

    Administrative Routes

--- src/Application.php
+++ src/Application.php
@@ -21,6 +21,7 @@ use Zend\Diactoros\Response\RedirectResponse;
 class Application
 {
     private $serviceContainer;
+    private $befores = [];
 
     /**
      * Application constructor.
@@ -65,19 +66,38 @@ class Application
             return $this;
     }
 
-    public function redirect($path)
+    public function redirect($path):ResponseInterface
     {
         return new RedirectResponse($path);
     }
 
-    public function route(string $name, array $params =[])
+    public function route(string $name, array $params =[]): ResponseInterface
     {
         $generator = $this->service('routing.generator');
         $path = $generator->generate($name, $params);
         return $this->redirect($path);
     }
 
-    public function start(){
+    public function before(callable $callback): Application
+    {
+        array_push($this->befores, $callback);
+        return $this;
+    }
+
+    protected function runBefores(): ?ResponseInterface
+    {
+        foreach ($this->befores as $callback){
+            $result = $callback($this->service(ResponseInterface::class));
+            if($result instanceof  ResponseInterface)
+            {
+                return $result;
+            }
+        }
+        return null;
+    }
+
+    public function start(): void
+    {
         $route = $this->service('route');
         /** @var ServerRequestInterface $request */
         $request = $this->service(RequestInterface::class);
@@ -93,12 +113,17 @@ class Application
             $request = $request->withAttribute($key, $value);
         }
 
+        $result = $this->runBefores();
+        if($result){
+            $this->emitResponse($result);
+            return;
+        }
         $callable = $route->handler;
         $response = $callable($request);
         $this->emitResponse($response);
     }
 
-    protected function emitResponse(ResponseInterface $response)
+    protected function emitResponse(ResponseInterface $response): void
     {
         $emitter = new SapiEmitter();
         $emitter->emit($response);
